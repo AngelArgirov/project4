3ad591442ce03999f983f38d23d1bb592f5d7f6b
AngelArgirov <awargirov@gmail.com>
2022-02-04 22:27:22 +0000

    payment integration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CSRF_TRUSTED_ORIGINS = ["https://8000-angelargirov-project4-k73slpjwz2h.ws-eu30.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = ["https://8000-angelargirov-project4-huqoqg1s9ja.ws-eu30.gitpod.io"]
 
 AUTHENTICATION_BACKENDS = [
     
