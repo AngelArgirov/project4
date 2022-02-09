d1487ba95d506ffaeab13883f8e04ce436ea32d1
AngelArgirov <awargirov@gmail.com>
2022-02-09 15:04:46 +0000

    new

--- store/admin.py
+++ store/admin.py
@@ -3,11 +3,9 @@ from .models import *
 
 # Register your models here.
 
-from .models import Room, Message
 
-# Register your models here.
-admin.site.register(Room)
-admin.site.register(Message)
+
+
 
 admin.site.register(Customer)
 admin.site.register(Product)
